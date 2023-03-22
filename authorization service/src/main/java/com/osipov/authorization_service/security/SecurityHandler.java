705bb4f30dd9b8ab84c84d9ac41018f02a13ad81
Vladislav <vladshelkovo@yandex.ru>
2023-03-22 08:58:07 +0300

    fix security

---
+++ authorization service/src/main/java/com/osipov/authorization_service/security/SecurityHandler.java
@@ -0,0 +1,26 @@
+package com.osipov.authorization_service.security;
+
+import java.io.IOException;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityHandler implements AuthenticationSuccessHandler {
+
+    public void onAuthenticationSuccess(HttpServletRequest request,   HttpServletResponse response, Authentication authentication) throws IOException  {
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        if (roles.contains("ADMIN")) {
+            response.sendRedirect("http://localhost:8080/");
+        }
+        if (roles.contains("USER")) {
+            response.sendRedirect("http://localhost:8080/");
+        }
+    }
+}
