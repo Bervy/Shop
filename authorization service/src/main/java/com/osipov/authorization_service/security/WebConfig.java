4bf0c1a5499b33027af013be61225a507e25b201
Vladislav <vladshelkovo@yandex.ru>
2023-03-22 09:22:23 +0300

    fix security

--- authorization service/src/main/java/com/osipov/authorization_service/security/WebConfig.java
+++ authorization service/src/main/java/com/osipov/authorization_service/security/WebConfig.java
@@ -10,10 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    public void addViewController(ViewControllerRegistry registry) {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/registration").setViewName("registration");
     }
 
     @Override
